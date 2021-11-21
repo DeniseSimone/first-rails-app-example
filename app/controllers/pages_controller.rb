c8d1b6b09fd4cc349c8f38a7539906ed00f048a0
denise custodio <denisesimone1997@gmail.com>
2021-11-21 16:41:13 +0000

    update controllers, views and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
+  def home
+    @time = Date.today.strftime("%B #{Date.today.day.ordinalize} ")
+  end
+
   def about
   end
+
+  def contact
+    search = params[:member]
+    @members = ['Jake', 'Max', 'Lori', 'Julian']
+
+    if search.present?
+      @members = @members.select { |name| name.start_with?(search) }
+    end
+
+  end
 end
+
+
